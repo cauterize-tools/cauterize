0f0b0e9db4b4a2ea93d967841589a2ada531c686
John Van Enk <vanenkj@gmail.com>
2014-04-29 13:16:28 -0400

    WIP - expanding out conversion from Schema to Spec types.

--- Cauterize/Specification.hs
+++ Cauterize/Specification.hs
@@ -5,8 +5,11 @@ module Cauterize.Specification
 
 import Cauterize.Specification.Types
 import Cauterize.FormHash
-
+import Cauterize.Common.BuiltIn
 import qualified Cauterize.Schema.Types as SC
+
+import Data.Bits
+import Data.List
 
 fromSchema :: SC.Schema -> Specification
 fromSchema (SC.Schema n v fs) =
@@ -16,4 +19,32 @@ fromSchema (SC.Schema n v fs) =
 fromSchemaForms :: [SC.SchemaForm] -> [SpecForm]
 fromSchemaForms = map fromSchemaForm
   where
-    fromSchemaForm (SC.FType f) = SpecForm
+    fromSchemaForm (SC.FType f) = SpecForm $ fromSchemaType f
+
+fromSchemaType :: SC.Type -> Type
+fromSchemaType (SC.TBuiltIn b) = TBuiltIn b
+fromSchemaType (SC.TScalar n b) = TScalar n b
+fromSchemaType (SC.TConst n b i) = TConst n b i
+fromSchemaType (SC.TFixedArray n m i) = TFixedArray n m i
+fromSchemaType (SC.TBoundedArray n m i) = TBoundedArray n m i (minimalExpression i)
+fromSchemaType (SC.TStruct n fs) = TStruct n (fromSchemaStructFields fs)
+fromSchemaType (SC.TSet n fs) = TSet n (minimalBitField $ length fs) (fromSchemaSetFields fs)
+fromSchemaType (SC.TEnum n vs) = TEnum n (minimalExpression $ length vs) (fromSchemaEnumVariants vs)
+
+fromSchemaStructFields :: [SC.StructField] -> [StructField]
+fromSchemaStructFields = map go
+  where
+    go (SC.StructField n t) = StructField n t
+
+fromSchemaSetFields :: [SC.SetField] -> [SetField]
+fromSchemaSetFields fs = snd $ mapAccumL go 0 fs
+  where
+    go :: Int -> SC.SetField -> (Int, SetField)
+    go a (SC.SetField n m) = (a + 1, SetField n m (1 `shiftL` a))
+
+fromSchemaEnumVariants :: [SC.EnumVariant] -> [EnumVariant]
+fromSchemaEnumVariants vs = snd $ mapAccumL go 0 vs
+  where
+    go :: Int -> SC.EnumVariant -> (Int, EnumVariant)
+    go a (SC.EnumVariant n m) = (a + 1, EnumVariant n m (fromIntegral a))
+
--- Cauterize/Specification/Types.hs
+++ Cauterize/Specification/Types.hs
@@ -1,6 +1,7 @@
 module Cauterize.Specification.Types where
 
 import Cauterize.FormHash
+import Cauterize.Common.BuiltIn
 
 data Specification = Specification
   { name :: String
@@ -10,7 +11,31 @@ data Specification = Specification
   }
   deriving (Show)
 
-data SpecForm = SpecForm
+data SpecForm = SpecForm Type
+  deriving (Show)
+
+data Type = TBuiltIn BuiltIn
+          | TScalar String BuiltIn
+          | TConst String BuiltIn Integer
+
+          | TFixedArray String String Integer
+          | TBoundedArray String String Integer BuiltIn
+
+          | TStruct String [StructField]
+          | TSet String BuiltIn [SetField]
+
+          | TEnum String BuiltIn [EnumVariant]
+
+          | TOther
+  deriving (Show)
+
+data StructField = StructField String String
+  deriving (Show)
+
+data SetField = SetField String String Integer
+  deriving (Show)
+
+data EnumVariant = EnumVariant String (Maybe String) Integer
   deriving (Show)
 
 instance Hashable Specification where
