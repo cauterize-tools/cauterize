3bc7a9555f24aad5fe5f72131f6048e7aa5387d6
John Van Enk <vanenkj@gmail.com>
2014-05-01 21:55:45 -0400

    Cleanup

--- Cauterize/Specification.hs
+++ Cauterize/Specification.hs
@@ -1,9 +1,8 @@
 module Cauterize.Specification
   ( module Cauterize.Specification.Types
   , fromSchema
-  , references
   , schemaTypeMap
-  , schemaTypeIdRefMap
+  , schemaTypeIdMap
   ) where
 
 import Cauterize.Specification.Types
@@ -15,15 +14,12 @@ import qualified Cauterize.Schema.Types as SC
 import Data.Bits
 import Data.List
 import Data.Maybe
-import Control.Monad
 import qualified Data.Map as M
 import qualified Data.ByteString as B
 
--- import qualified Data.Graph.SCC
 import Data.Graph
 
 type Name = String
-type Error = String
 
 fromSchema :: SC.Schema -> Maybe Specification
 fromSchema (SC.Schema n v fs) =
@@ -35,80 +31,31 @@ fromSchemaForms = map fromSchemaForm
   where
     fromSchemaForm (SC.FType f) = SpecForm $ fromSchemaType f
 
-schemaTypeIdRefMap :: M.Map Name SC.Type -> (Either [[Name]] (M.Map Name FormHash))
-schemaTypeIdRefMap m = case cycs of
+schemaTypeIdMap :: M.Map Name SC.Type -> Either [[Name]] (M.Map Name FormHash)
+schemaTypeIdMap m = case cycs of
                           [] -> Right $ fmap hashType m
                           cs -> Left cs
   where
-    -- refs = concat $ maybeToList $ m `references` n
     cycs = cycles (map snd $ M.toList m)
     r = fmap hashType m
 
     hashType :: SC.Type -> FormHash
     hashType t = result
       where
-        n = cautName t
-        th = formHashCtx t
-
         -- YO! There's a fromJust here. The way the input map is constructed
         -- should keep us from having to worry about this.
-        -- dirRefs :: [Either [Name] FormHash]
-        dirRefs :: [FormHash]
         dirRefs = fromJust $ mapM (`M.lookup` r) (referredNames t)
-
-        -- result = foldM formHashWith th dirRefs
-        result :: FormHash
-        result = finalize $ foldl formHashWith th dirRefs
-
-        -- refHashs
-        -- dirRefs = mapM (liftM fst . (`M.lookup` r)) (referredNames t) >>= sequence >>= magicHash -- :: Maybe [FormHash]
+        result = finalize $ foldl formHashWith (formHashCtx t) dirRefs
 
 cycles :: [SC.Type] -> [[Name]]
-cycles ts = let ns = map cautName ts
-                ns' = map (\t -> (cautName t, cautName t, referredNames t)) ts
-
-            in mapMaybe isScc (stronglyConnComp ns')
+cycles ts = let ns = map (\t -> (cautName t, cautName t, referredNames t)) ts
+            in mapMaybe isScc (stronglyConnComp ns)
   where
     isScc (CyclicSCC vs) = Just vs
     isScc _ = Nothing
 
-            
-
-cycCheck :: [Name] -> Maybe [Name]
-cycCheck ns = go ns []
-  where
-    go [] _ = Nothing
-    go (i:rs) prev = if i `elem` prev
-                        then Just (i:prev)
-                        else go rs (i:prev)
-
-
--- schemaTypeIdRefMap :: M.Map Name SC.Type -> M.Map Name (Maybe (FormHash, [Name]))
--- schemaTypeIdRefMap m = fmap hashType m
---   where
---     hashType :: SC.Type -> Maybe (FormHash, [Name])
---     hashType t = do
---       refs <- references m (cautName t)
---       return (undefined, refs)
-
 schemaTypeMap :: SC.Schema -> M.Map Name SC.Type
 schemaTypeMap (SC.Schema _ _ fs) = M.fromList $ map (\(SC.FType t) -> (cautName t, t)) fs
-
-references :: M.Map Name SC.Type -> Name -> Maybe [Name]
-references m n = do
-  t <- n `M.lookup` m
-
-  let rns = referredNames t
-  let refs = map (maybeToList . references m) rns
-
-  return $ n : (concat . concat) refs
-
--- schemaTypeInfo :: M.Map Name SC.Type -> M.Map Name (Either Error FormHash, [Name])
--- schemaTypeInfo = fmap hashRefs
---   where
---     hashRefs :: SC.Type -> (Either Error FormHash, [Name])
---     hashRefs t = let n = cautName n
---                  in 
 
 fromSchemaType :: SC.Type -> Type
 fromSchemaType (SC.TBuiltIn b) = TBuiltIn b
@@ -144,9 +91,3 @@ fromSchemaPartialVariants = map go
   where
     go :: SC.PartialVariant -> PartialVariant
     go (SC.PartialVariant n t) = PartialVariant n t (FormHash $ B.pack [0])
-
--- import Data.Maybe
--- uniquePrefixes :: Eq a => [[a]] -> Maybe [[a]]
--- uniquePrefixes ls = listToMaybe . dropper . map nub . transpose . map inits $ ls
---   where
---     dropper = dropWhile (\l -> length l < length ls) 
