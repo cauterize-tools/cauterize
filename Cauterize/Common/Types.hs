a25143d2f321cb152d004ecf61eb01966df1ac08
John Van Enk <vanenkj@gmail.com>
2014-07-19 23:28:48 -0700

    Moosh all these files into one.

--- Cauterize/Common/Types.hs
+++ Cauterize/Common/Types.hs
@@ -1,12 +1,88 @@
-module Cauterize.Common.Types (module X) where
+{-# LANGUAGE FlexibleInstances, DeriveDataTypeable #-}
 
-import Cauterize.Common.Types.BuiltIn as X
-import Cauterize.Common.Types.Scalar as X
-import Cauterize.Common.Types.Const as X
-import Cauterize.Common.Types.FixedArray as X
-import Cauterize.Common.Types.BoundedArray as X
-import Cauterize.Common.Types.Struct as X
-import Cauterize.Common.Types.Set as X
-import Cauterize.Common.Types.Enum as X
-import Cauterize.Common.Types.Partial as X
-import Cauterize.Common.Types.Pad as X
+module Cauterize.Common.Types where
+
+import Cauterize.Common.Primitives
+import Cauterize.Common.References
+import Cauterize.Common.IndexedRef
+import Data.Data
+import Data.List
+
+data TBuiltIn = TBuiltIn { unTBuiltIn :: BuiltIn }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References TBuiltIn where
+  referencesOf _ = []
+
+
+data TConst = TConst { constName :: Name
+                     , constRepr :: BuiltIn
+                     , constValue :: Integer }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References TConst where
+  referencesOf (TConst _ b _) = [show b]
+
+
+data TFixedArray t = TFixedArray { fixedArrName :: Name,
+                                   fixedArrRef :: t,
+                                   fixedArrLen :: Integer }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References (TFixedArray Name) where
+  referencesOf (TFixedArray _ n _) = [n]
+
+
+data TBoundedArray t = TBoundedArray { boundedArrName :: Name
+                                     , boundedArrRef :: t
+                                     , boundedArrMaxLen :: Integer
+                                     }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References (TBoundedArray Name) where
+  referencesOf (TBoundedArray _ n _) = [n]
+
+
+data TScalar = TScalar { scalarName :: Name
+                       , scalarRepr :: BuiltIn }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References TScalar where
+  referencesOf (TScalar _ b) = [show b]
+
+
+data TStruct t = TStruct { structName :: Name, structFields :: Fields t }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References (TStruct Name) where
+  referencesOf (TStruct _ (Fields rs)) = nub $ map refRef rs
+
+
+data TEnum t = TEnum { enumName :: Name, enumFields :: Fields t }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References (TEnum Name) where
+  referencesOf (TEnum _ (Fields rs)) = nub $ map refRef rs
+
+
+data TSet t = TSet { setName :: Name, setFields :: Fields t }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References (TSet Name) where
+  referencesOf (TSet _ (Fields rs)) = nub $ map refRef rs
+
+
+data TPartial t = TPartial { partialName :: Name, partialFields :: Fields t }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References (TPartial Name) where
+  referencesOf (TPartial _ (Fields rs)) = nub $ map refRef rs
+
+
+data TPad = TPad { padName :: Name, padLength :: Integer }
+  deriving (Show, Ord, Eq, Data, Typeable)
+
+instance References TPad where
+  referencesOf (TPad _ _) = []
+
+
