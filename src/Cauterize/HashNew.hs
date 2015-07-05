290bf6b1d004c74f0d0454a6140c124227485295
John Van Enk <vanenkj@gmail.com>
2015-07-05 10:30:35 -0700

    Add a null hash and a way to make a hash from a hex string of 20 bytes.

--- src/Cauterize/HashNew.hs
+++ src/Cauterize/HashNew.hs
@@ -1,8 +1,10 @@
 module Cauterize.HashNew
   ( Hash(unHash)
   , mkHash
+  , mkHashFromHexString
   , hashToHex
   , hashToBytes
+  , hashNull
   ) where
 
 import Numeric
@@ -19,6 +21,17 @@ mkHash :: T.Text -> Hash
 mkHash t = let h = SHA1.init `SHA1.update` T.encodeUtf8 t
            in Hash (SHA1.finalize h)
 
+mkHashFromHexString :: T.Text -> Hash
+mkHashFromHexString t | T.length t == 20 && (`elem` validChars) `T.all` t = go
+                      | otherwise = error "Unable to create hash. String wrong length."
+  where
+    validChars = ['0'..'9'] ++ ['a'..'f'] ++ ['A'..'F']
+    go = Hash . B.pack $ map (fromHex . T.unpack) (T.chunksOf 2 t)
+    fromHex hbyte = case readHex hbyte of
+                      [(n,"")] -> n
+                      _ -> error $ "mkHashFromHexString: Unable to read hex byte '" ++ hbyte ++ "'."
+
+
 hashToHex :: Hash -> T.Text
 hashToHex (Hash bs) = T.concat $ map showByte (B.unpack bs)
   where
@@ -29,6 +42,9 @@ hashToHex (Hash bs) = T.concat $ map showByte (B.unpack bs)
 
 hashToBytes :: Hash -> [Word8]
 hashToBytes (Hash b) = B.unpack b
+
+hashNull :: Hash
+hashNull = Hash $ B.pack (replicate 20 0)
 
 instance Show Hash where
   show h = "SHA1:" ++ T.unpack (hashToHex h)
