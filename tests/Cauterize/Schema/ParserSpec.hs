99c3421c5df54d1e6f06396895f6c5c46e8f8994
John Van Enk <vanenkj@gmail.com>
2015-09-25 15:26:50 -0500

    Update the tests to reflect the swapped order for the Schema.

--- tests/Cauterize/Schema/ParserSpec.hs
+++ tests/Cauterize/Schema/ParserSpec.hs
@@ -14,8 +14,23 @@ spec :: Spec
 spec = do
   describe "parseSchema" $ do
     it "parses a synonym" $ do
-      let s = parseSchema "(type synonym foo u8)"
+      let s = parseSchema "(type foo synonym u8)"
       s `shouldSatisfy` isRight
       s `shouldSatisfy` hasSynNamedFoo
+    it "parses a formatted schema" $ do
+      let r = do
+            s <- parseSchema "(type foo synonym u8)"
+            let f = formatSchema s
+            s' <- parseSchema f
+            return (s == s')
+      r `shouldBe` (Right True)
   where
-    hasSynNamedFoo (Right (Schema { schemaTypes = [Type { typeName = n, typeDesc = Synonym _ }] })) = unIdentifier n == "foo"
+    hasSynNamedFoo
+      (Right
+       (Schema {
+            schemaTypes = [Type {
+                               typeName = n,
+                               typeDesc = Synonym _
+                               }]
+            })) = unIdentifier n == "foo"
+    hasSynNamedFoo _ = False
